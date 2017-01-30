b908da1e01d346184d67f6c7c8f7f6fc9fd42ea9
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-29 21:44:03 -0600

    finish set form minutes

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+// grab all buttons
+const buttons = document.querySelectorAll('[data-time]');
 
 
 function timer(seconds) {
+  //clear any exist timers
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -40,3 +44,18 @@ function displayEndTime(timestamp) {
   const minutes = end.getMinutes();
   endTime.textContent = `Be back at ${adjustHour}:${minutes < 10 ? '0':''}${minutes}`;
 }
+
+
+function startTimer(){
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  // console.log(mins);
+  timer(mins * 60);
+  this.reset();
+})
