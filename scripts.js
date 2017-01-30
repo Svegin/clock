1b04508d3d0787b1b3e622ac5c322a5841048955
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-29 21:04:39 -0600

    timer work

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now())/ 1000);
     // check if we should stop it
-    if (secondsLeft <=0) {
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
       return;
     }
     //display it
-    console.log(secondsLeft);
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  // add 0 when seconds < 10
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+//display remaining time in title of page
+  document.title = display;
+  // push to display current remaining time
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${adjustHour}:${minutes < 10 ? '0':''}${minutes}`;
 }
